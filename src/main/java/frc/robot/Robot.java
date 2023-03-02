665349bfeb0988853637e74037f68e59726a3b7e
acastagna <acastagna@albany.k12.ny.us>
2023-03-02 07:25:51 -0500

    Add Arm Default

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,6 @@
 
 package frc.robot;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -9,7 +8,6 @@ import java.util.Date;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.StatusFrame;
@@ -20,10 +18,6 @@ import com.ctre.phoenix.sensors.AbsoluteSensorRange;
 import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix.sensors.CANCoderStatusFrame;
 
-import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.util.datalog.DataLog;
-import edu.wpi.first.util.datalog.DoubleLogEntry;
-import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -38,16 +32,15 @@ public class Robot extends TimedRobot {
   DigitalInput limitUpper = new DigitalInput(0);
   DigitalInput limitLower = new DigitalInput(1);
   Joystick joy = new Joystick(0);
-  ArmFeedforward feedFwd;
-  double absPos, relPos;
-  double armkP = 0.2, armkD = 0, armkI = 0, armkIZone, armkF = 0;
-  double downkP = 0.15, downkD = 1, downkI = 0, downIZone, downkF = 0;
-  double armkG = 0.05, armkS = 0.005, armkV = 0, armkA = 0;
-  double downwardskG = 0;
-  double armPos1 = 1170, armPos2 = 1436, armPos3 = 2081;
+  double armkP = 1.7, armkD = 80, armkI = 0.0012, armkIZone=70, armkF = 0;
+  double armkPdown = 0.15, armkDdown = 10, armkIdown = 2, armkIZonedown, armkFdown = 0;
+  double armkG = 0.0721, armkS =-0.0138, armkV = 0, armkA = 0;
+  double armkG2 = 0.05, armkS2 =-0.01, armkV2 = 0, armkA2 = 0;
+  double armkG3 = 0.0721, armkS3 =-0.0138, armkV3 = 0, armkA3 = 0;
+  double armPos1 = 1200, armPos2 = 1309, armPos3 = 2400;
   double armMMVel = 120, armMMAcc = 100;
-  double armForwardSensorLim = 2600, armReverseSensorLim = 1127;
-  double armMaxOutput = .4;
+  double armForwardSensorLim = 2600, armReverseSensorLim = 1140;
+  double armMaxOutput = .125;
 
   double angle, angleCounts;
   double angleFromHorizontalDeg, angleFromHorizontalCounts, angleFromHorizontalRad;
@@ -57,48 +50,39 @@ public class Robot extends TimedRobot {
   double angleRate, angleRatePrev = 0, angleRate2;
   double voltage;
   double time = 0, timePrev = 0;
-
+  int button = 0;
   double stick;
   boolean logging = false, loggingPrev = false;
   boolean ls_upper = true, ls_lower = false, ls_upperActive = false;
-  DoubleLogEntry voltageLog, angleLog, angleRateLog, angleRate2Log, cosAngleLog, signAngleRateLog;
-  DataLog log;
   FileWriter writer;
+  double voltageauto=0,autoTime;
+  boolean autoFlagA=false,autoFlagB=false;
 
   public Robot() {
-    super(0.005);
+    super(0.02);
     LiveWindow.disableAllTelemetry();
   }
 
   @Override
   public void robotInit() {
 
-    /*
-     * DataLogManager.start();
-     * log = DataLogManager.getLog();
-     * 
-     * voltageLog = new DoubleLogEntry(log, "voltage");
-     * angleLog = new DoubleLogEntry(log, "angle");
-     * angleRateLog = new DoubleLogEntry(log, "angleRate");
-     * angleRate2Log = new DoubleLogEntry(log, "angleRate2");
-     * cosAngleLog = new DoubleLogEntry(log, "cosAngle");
-     * signAngleRateLog = new DoubleLogEntry(log, "signAngleRate");
-     */
-
     try {
       String fileName = "/home/lvuser/data" + new SimpleDateFormat("yyyyMMddhhmm'.txt'").format(new Date());
-      writer = new FileWriter(fileName);
+        writer = new FileWriter(fileName);
+        writer.write("time,voltage,angleCounts,rate,rate2");
+        writer.write("\n");
     } catch (IOException e) {
-      System.out.println("An error occurred.");
+      System.out.println("Error opening log file");
       e.printStackTrace();
     }
 
     enc.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
     enc.setPositionToAbsolute(20);
     enc.configSensorDirection(true);
-    enc.setStatusFramePeriod(CANCoderStatusFrame.SensorData, 10, 20);
+    enc.setStatusFramePeriod(CANCoderStatusFrame.SensorData, 5, 20);
 
     armMotor.configFactoryDefault();
+    
 
     // set voltage compensation
     armMotor.configVoltageCompSaturation(11.5);
@@ -109,13 +93,14 @@ public class Robot extends TimedRobot {
     armMotor.configReverseSoftLimitThreshold(armReverseSensorLim);
     armMotor.configForwardSoftLimitEnable(true);
     armMotor.configReverseSoftLimitEnable(true);
+    // armM
 
     // set to brake mode
-    armMotor.setNeutralMode(NeutralMode.Coast);
+    armMotor.setNeutralMode(NeutralMode.Brake);
 
-    // set feedback sensor to cancoder
+    // set feedbak sensor to cancoder
     armMotor.configRemoteFeedbackFilter(enc, 0);
-    armMotor.configSelectedFeedbackSensor(FeedbackDevice.RemoteSensor0, 0, 20);
+    armMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.RemoteSensor0, 0, 20);
 
     // set max closed loop output
     armMotor.configClosedLoopPeakOutput(0, armMaxOutput);
@@ -123,11 +108,13 @@ public class Robot extends TimedRobot {
     armMotor.configPeakOutputReverse(-armMaxOutput);
 
     // set current limit
-    StatorCurrentLimitConfiguration currentConfig = new StatorCurrentLimitConfiguration(true, 20, 25, 1);
-    // armMotor.configGetStatorCurrentLimit(currentConfig) ;
+    StatorCurrentLimitConfiguration currentConfig = 
+        new StatorCurrentLimitConfiguration(true, 30, 
+        32, .1);
+    armMotor.configStatorCurrentLimit(currentConfig);
 
-    armMotor.setStatusFramePeriod(StatusFrame.Status_10_Targets, 10);
-    armMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10);
+    armMotor.setStatusFramePeriod(StatusFrame.Status_10_Targets, 5);
+    armMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 5);
 
     SmartDashboard.putNumber("arm kP", armkP);
     SmartDashboard.putNumber("arm kI", armkI);
@@ -135,16 +122,28 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("arm kF", armkF);
     SmartDashboard.putNumber("arm kIzone", armkIZone);
 
-    SmartDashboard.putNumber("down kP", downkP);
-    SmartDashboard.putNumber("down kI", downkI);
-    SmartDashboard.putNumber("down kD", downkD);
-    SmartDashboard.putNumber("down kF", downkF);
-    SmartDashboard.putNumber("down kIzone", downIZone);
+    SmartDashboard.putNumber("arm kP down", armkPdown);
+    SmartDashboard.putNumber("arm kI down ", armkIdown);
+    SmartDashboard.putNumber("arm kD down", armkDdown);
+    SmartDashboard.putNumber("arm kF down", armkFdown);
+    SmartDashboard.putNumber("arm kIzone down", armkIZonedown);
 
     SmartDashboard.putNumber("arm kG", armkG);
     SmartDashboard.putNumber("arm kS", armkS);
     SmartDashboard.putNumber("arm kV", armkV);
     SmartDashboard.putNumber("arm kA", armkA);
+
+    SmartDashboard.putNumber("arm kG2", armkG2);
+    SmartDashboard.putNumber("arm kS2", armkS2);
+    SmartDashboard.putNumber("arm kV2", armkV2);
+    SmartDashboard.putNumber("arm kA2", armkA2);
+
+    SmartDashboard.putNumber("arm kG3", armkG3);
+    SmartDashboard.putNumber("arm kS3", armkS3);
+    SmartDashboard.putNumber("arm kV3", armkV3);
+    SmartDashboard.putNumber("arm kA3", armkA3);
+
+
     SmartDashboard.putNumber("arm pos1", armPos1);
     SmartDashboard.putNumber("arm pos2", armPos2);
     SmartDashboard.putNumber("arm pos3", armPos3);
@@ -153,7 +152,6 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("arm ForSensorLim", armForwardSensorLim);
     SmartDashboard.putNumber("arm RevSensorLim", armReverseSensorLim);
     SmartDashboard.putNumber("arm MaxOutput", armMaxOutput);
-    SmartDashboard.putNumber("armkG (down)", downwardskG);
 
     SmartDashboard.putBoolean("logging", false);
 
@@ -167,7 +165,7 @@ public class Robot extends TimedRobot {
     ls_lower = limitLower.get();
 
     stick = joy.getRawAxis(0);
-    if (Math.abs(stick) < 0.05)
+    if (Math.abs(stick) < 0.02)
       stick = 0;
     isUpperLimitActive(stick);
 
@@ -185,8 +183,11 @@ public class Robot extends TimedRobot {
 
     angleRate2 = (angleRate - angleRatePrev) / (time - timePrev);
 
+    //System.out.println(time+"  "+(time-timePrev)+"   "+angleRate+"   "+angleRatePrev);
+
     angleRatePrev = angleRate;
     timePrev = time;
+
 
     SmartDashboard.putNumber("arm Pos Counts", angleCounts);
     SmartDashboard.putNumber("arm Pos Deg", angle);
@@ -199,42 +200,42 @@ public class Robot extends TimedRobot {
 
     SmartDashboard.putNumber("arm voltage", voltage);
     SmartDashboard.putNumber("arm current", armMotor.getStatorCurrent());
-    SmartDashboard.putNumber("arm current", armMotor.getStatorCurrent());
     SmartDashboard.putNumber("arm CLE", armMotor.getClosedLoopError());
     SmartDashboard.putBoolean("arm Upper LS", ls_upper);
     SmartDashboard.putBoolean("arm Lower LS", ls_lower);
     logging = SmartDashboard.getBoolean("logging", false);
 
-    /*
-     * // toggle logging on and off
-     * if (logging && !loggingPrev) log.resume();
-     * if(!logging && loggingPrev) log.pause();
-     * loggingPrev=logging;
-     * 
-     * 
-     * // append to log , takes no action if logging is paused
-     * voltageLog.append(voltage);
-     * angleLog.append(angleCounts);
-     * cosAngleLog.append(cosAngleFromHorizontal);
-     * signAngleRateLog.append(Math.signum(angleRate));
-     * angleRateLog.append(angleRate);
-     * angleRate2Log.append(angleRate2);
-     */
-
-    if (logging) {
-      try {
-        String txt = Double.toString(time) + "," +
-            Double.toString(voltage) + "," +
-            Double.toString(angleCounts) +
-            Double.toString(angleFromHorizontalCounts) + "," +
-            Double.toString(angleRate) + "," + "\n";
-        writer.write(txt);
-      } catch (IOException e) {
-        System.out.println("An error occurred.");
-        e.printStackTrace();
-      }
-    }
+//    if (logging) logData();
   }
+
+
+  @Override
+  public void autonomousInit() {
+      autoFlagA=false;autoFlagB=false;voltageauto=-0.04;
+      armMotor.set(ControlMode.PercentOutput,voltageauto);
+      Timer.delay(2);
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        
+        if (angleCounts>2600) autoFlagA=true;
+        if (angleCounts<1100) autoFlagB=true;
+
+        if (angleRate==0 && angleRate2==0 && Timer.getFPGATimestamp()-autoTime>2){
+          logData(); 
+          if (!autoFlagA) voltageauto+=0.0005;
+          if (autoFlagA && !autoFlagB) voltageauto -= 0.0005;
+          if (autoFlagB) voltageauto=0;
+          autoTime=Timer.getFPGATimestamp();          
+        } 
+//        System.out.println("FlagA = "+autoFlagA+"    FlagB = "+autoFlagB+"   voltAuto = "+voltageauto);
+        armMotor.set(ControlMode.PercentOutput,voltageauto);
+        SmartDashboard.putNumber("arm VoltAuto", voltageauto);
+      }
+
+
+
 
   /** This function is called once when teleop is enabled. */
   @Override
@@ -244,61 +245,40 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    double arbff = +armkG * Math.cos(angleFromHorizontalRad) + armkS;
+    double arbff;
+    if (angleFromHorizontalDeg < -69)
+      arbff = +armkG2 * Math.cos(angleFromHorizontalRad) + armkS2;
+    else if (angleFromHorizontalDeg >= -69 && angleFromHorizontalDeg<0)
+      arbff = +armkG * Math.cos(angleFromHorizontalRad) + armkS;
+    else
+      arbff = +armkG3 * Math.cos(angleFromHorizontalRad) + armkS3;
+
 
     if (joy.getRawButton(1)) {
-      // armMotor.set(ControlMode.MotionMagic,pos1,
-      // DemandType.ArbitraryFeedForward,feedFwd.calculate(angleFromHorizontalRad,
-      // 0));
-
-      double mapped = map(stick, -1, 1, 101, 220) / 360 * 4096;
-      SmartDashboard.putNumber("map", mapped);
-      armMotor.set(ControlMode.MotionMagic, mapped, DemandType.ArbitraryFeedForward, arbff);
+      if(button !=1){
+        button=1;
+        armMotor.setIntegralAccumulator(0, 0,20);
+      }
+//      armMotor.set(ControlMode.MotionMagic, armPos1, DemandType.ArbitraryFeedForward, arbff);
+armMotor.set(ControlMode.Position, armPos1, DemandType.ArbitraryFeedForward, arbff);
     }
 
     else if (joy.getRawButton(2)) {
-      // armMotor.set(ControlMode.MotionMagic,pos2,
-      // DemandType.ArbitraryFeedForward,feedFwd.calculate(angleFromHorizontalRad,
-      // 0));
-      if (angleCounts > armPos2) {
-
-        armMotor.config_kP(0, downkP);
-        armMotor.config_kI(0, downkI);
-        armMotor.config_kD(0, downkD);
-        armMotor.config_kF(0, downkF);
-        armMotor.config_IntegralZone(0, downIZone);
-      } else {
-
-        armMotor.config_kP(0, armkP);
-        armMotor.config_kI(0, armkI);
-        armMotor.config_kD(0, armkD);
-        armMotor.config_kF(0, armkF);
-        armMotor.config_IntegralZone(0, armkIZone);
-      }
-      armMotor.set(ControlMode.MotionMagic, armPos2, DemandType.ArbitraryFeedForward, arbff);
-
-      // System.out.println("target = " + armMotor.getClosedLoopTarget() +
-      // " CLE = " + armMotor.getClosedLoopError());
-
+     if(button !=2){
+      button=2;
+      armMotor.setIntegralAccumulator(0, 0,20);
+    }
+//    armMotor.set(ControlMode.MotionMagic, armPos2, DemandType.ArbitraryFeedForward, arbff);
+armMotor.set(ControlMode.Position, armPos2, DemandType.ArbitraryFeedForward, arbff);
     }
 
-    else if (joy.getRawButton(3)) {
-      if (angleCounts > armPos3) {
-
-        armMotor.config_kP(0, downkP);
-        armMotor.config_kI(0, downkI);
-        armMotor.config_kD(0, downkD);
-        armMotor.config_kF(0, downkF);
-        armMotor.config_IntegralZone(0, downIZone);
-      } else {
-
-        armMotor.config_kP(0, armkP);
-        armMotor.config_kI(0, armkI);
-        armMotor.config_kD(0, armkD);
-        armMotor.config_kF(0, armkF);
-        armMotor.config_IntegralZone(0, armkIZone);
+    else if (joy.getRawButton(3)) {       
+      if(button !=3){
+        button=3;
+        armMotor.setIntegralAccumulator(0, 0,20);
       }
-      armMotor.set(ControlMode.MotionMagic, armPos3, DemandType.ArbitraryFeedForward, arbff);
+//      armMotor.set(ControlMode.MotionMagic, armPos3, DemandType.ArbitraryFeedForward, arbff);
+armMotor.set(ControlMode.Position, armPos3, DemandType.ArbitraryFeedForward, arbff);
     }
 
     else if (joy.getRawButton(4)) {
@@ -307,27 +287,22 @@ public class Robot extends TimedRobot {
 
     else {
       // if (!ls_upperActive)
-      // if (!ls_upperActive) armMotor.set(ControlMode.PercentOutput, demand
-      // ,DemandType.ArbitraryFeedForward,feedFwd.calculate(angleFromHorizontalRad,
-      // 0));
-
-      double demand = (stick * .2) + armkG * Math.cos(angleFromHorizontalRad) + armkS;
-      SmartDashboard.putNumber("demand", demand);
+      double demand = (stick * .2) + arbff;
       armMotor.set(ControlMode.PercentOutput, demand);
     }
-    // double demand = (stick*.2) + cosAngleFromHorizontal*.0762;
-    // 0.0762*x + 0.0231
 
   }
 
   public void updateConstants() {
     armkG = SmartDashboard.getNumber("arm kG", 0);
-    downwardskG = SmartDashboard.getNumber("armkG (down)", 0);
-    SmartDashboard.getNumber("arm kS", 0);
+    armkS=SmartDashboard.getNumber("arm kS", 0);
 
-    feedFwd = new ArmFeedforward(armkS, armkG,
-        SmartDashboard.getNumber("arm kV", 0),
-        SmartDashboard.getNumber("arm kA", 0));
+    armkG2 = SmartDashboard.getNumber("arm kG2`", 0);
+    armkS2=SmartDashboard.getNumber("arm kS2", 0);
+
+    armkG3 = SmartDashboard.getNumber("arm kG3`", 0);
+    armkS3=SmartDashboard.getNumber("arm kS3", 0);
+
 
     armkP = SmartDashboard.getNumber("arm kP", 0);
     armkI = SmartDashboard.getNumber("arm kI", 0);
@@ -335,17 +310,23 @@ public class Robot extends TimedRobot {
     armkF = SmartDashboard.getNumber("arm kF", 0);
     armkIZone = SmartDashboard.getNumber("arm kIzone", 0);
 
-    downkP = SmartDashboard.getNumber("down kP", 0);
-    downkI = SmartDashboard.getNumber("down kI", 0);
-    downkD = SmartDashboard.getNumber("down kD", 0);
-    downkF = SmartDashboard.getNumber("down kF", 0);
-    downIZone = SmartDashboard.getNumber("down kIzone", 0);
+    armkPdown = SmartDashboard.getNumber("arm kP down", 0);
+    armkIdown = SmartDashboard.getNumber("arm kI down", 0);
+    armkDdown = SmartDashboard.getNumber("arm kD down", 0);
+    armkFdown = SmartDashboard.getNumber("arm kF down", 0);
+    armkIZonedown = SmartDashboard.getNumber("arm kIzone down", 0);
 
     armMotor.config_kP(0, armkP);
     armMotor.config_kI(0, armkI);
     armMotor.config_kD(0, armkD);
     armMotor.config_kF(0, armkF);
     armMotor.config_IntegralZone(0, armkIZone);
+
+    armMotor.config_kP(1, armkPdown);
+    armMotor.config_kI(1, armkIdown);
+    armMotor.config_kD(1, armkDdown);
+    armMotor.config_kF(1, armkFdown);
+    armMotor.config_IntegralZone(1, armkIZonedown);
 
     armMMVel = SmartDashboard.getNumber("arm MMvel", 0);
     armMMAcc = SmartDashboard.getNumber("arm MMacc", 0);
@@ -377,11 +358,21 @@ public class Robot extends TimedRobot {
       ls_upperActive = false;
   }
 
-  public double getFeedForward() {
-    return feedFwd.calculate(angleFromHorizontalDeg * Math.PI / 180, 0);
+  public void logData(){
+    try {
+      String txt = Double.toString(time) + "," +
+          Double.toString(voltageauto) + "," +
+          Double.toString(angleCounts) +","+
+          Double.toString(angleRate) + ","+ 
+          Double.toString(angleRate2) + ","+
+          "\n";
+      writer.write(txt);
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+
   }
 
-  public static double map(double i, double b1, double t1, double b2, double t2) {
-    return (i - b1) / (t1 - b1) * (t2 - b2) + b2;
-  }
+
 }
